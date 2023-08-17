7aec59fb3012c80e01c0a4ddc4c687cd2c625044
Daniel Kristianto Haryono <agni.infinity@gmail.com>
2023-08-17 22:52:05 +0700

    feat: add color vs grayscale

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('./images/lenna.png', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image', img)
+imcolor = cv2.imread('./images/lenna.png', cv2.IMREAD_COLOR)
+imgray = cv2.imread('./images/lenna.png', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow(
+    'Color -> Grayscale',
+    np.hstack((
+        imcolor,
+        cv2.merge((imgray, imgray, imgray))
+    ))
+)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
